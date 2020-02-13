484da0aeecc4bc3d92dff336da36d412157fcc73
Jefferson Geovanny <jeffgeomp@gmail.com>
2020-02-12 20:34:15 -0600

    Initial Commit

---
+++ src/olc1/practica1_201603047/OLC1Practica1_201603047.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olc1.practica1_201603047;
+
+/**
+ *
+ * @author Jeffe
+ */
+public class OLC1Practica1_201603047 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
